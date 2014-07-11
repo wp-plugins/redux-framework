015023a5966a5e50f0c3593051f5a8e6e5d69445
dovyp <dovyp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 01:45:30 +0000

    3.3.4.1
    git-svn-id: https://plugins.svn.wordpress.org/redux-framework/tags/3.3.4.1@946655 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ReduxCore/inc/class.redux_filesystem.php
@@ -0,0 +1,84 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if (!class_exists('Redux_Filesystem')){
+    class Redux_Filesystem {
+        private $parent     = null;
+        
+        public $fs_object   = null;
+        
+        public function __construct($parent) {
+            $this->parent = $parent;
+        }
+        
+        public function execute($action, $file, $params = '') {
+            global $wp_filesystem;
+            
+            if (!empty ($params)) {
+                extract($params);
+            }
+            
+            // Setup the filesystem with creds
+            require_once( ABSPATH . '/wp-admin/includes/file.php' );
+            
+            if ($this->parent->args['menu_type'] == 'submenu') {
+                $page_parent = $this->parent->args['page_parent'];
+                $base = $page_parent . '?page=' . $this->parent->args['page_slug'];
+            } else {
+                $base = 'admin.php?page=' . $this->parent->args['page_slug'];
+            }
+            
+            $url = wp_nonce_url($base);
+            if ( is_admin() && isset( $_GET['page'] ) && $_GET['page'] == $this->parent->args['page_slug'] ) {
+                $creds = request_filesystem_credentials($url, '', false, false);    
+            } else {
+                return;
+            }
+
+            if (false === $creds) {
+                if (!is_admin()) {
+                    return;
+                }
+                
+                return true;
+            }
+
+            if (!WP_Filesystem($creds)) {
+                if (!is_admin()) {
+                    return;
+                }
+                // Never load this unless we're on the panel page.
+                if ( is_admin() && isset( $_GET['page'] ) && $_GET['page'] == $this->parent->args['page_slug'] ) {
+                    $creds = request_filesystem_credentials($url, '', false, false);    
+                } else {
+                    return;
+                }
+                return true;
+            }   
+            
+            $wp_filesystem =& $wp_filesystem;
+            
+            // Do unique stuff
+            if ($action == 'mkdir') {
+                $res = $wp_filesystem->$action($file, 0755 );
+            } elseif ($action == 'copy') {
+                echo $wp_filesystem->copy($file, $destination, $overwrite, 0644 );
+                $res = false;
+            } elseif ($action == 'put_contents') {
+                $wp_filesystem->put_contents($file, $content, 0644);
+                $res = false;
+            } elseif ($action == 'get_contents') {
+                $res = $wp_filesystem->$action($file);
+            } elseif ($action == 'object') {
+                $res = $wp_filesystem;
+            } else {
+                $res = false;
+            }
+            
+            return $res;
+        }
+    }
+}
